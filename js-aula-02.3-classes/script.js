2b3d2210a95312989c699d1f7d8078174d196f72
marcelo250 <marcelonunesdeandrade267@gmail.com>
2026-08-21 20:10:27 -0300

    Aula-02.3

---
+++ js-aula-02.3-classes/script.js
@@ -0,0 +1,23 @@
+//CLASSES NO JAVASCRIPT
+class Carro {
+  //Nome atributo devem iniciar com a primeira letra maiúscula
+
+  //ATRIBUTOS
+  constructor(marca, modelo, ano) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.ano = ano;
+  }
+  //MÉTODOS
+  buzinar() {
+    return "Beep! Beep!";
+  }
+}
+
+//Criando uma instância (objeto) da classe carro
+
+const carroPopular = new Carro("Fiat", "Uno", "2012");
+//Acessando os ATRIBUTOS e MÉTODOS do objeto
+document.write(`
+    <p>O carro popular ${carroPopular.marca} modelo ${carroPopular.modelo} é do ano ${carroPopular.ano} e quando ele buzina faz ${carroPopular.buzinar()}</p>
+    `);
